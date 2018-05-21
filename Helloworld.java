025aac2546b3ced59a54ef2ef43d5797de492d89
staxbe <38501278+staxbe@users.noreply.github.com>
2018-05-21 23:18:04 +0800

    test

--- Helloworld.java
+++ Helloworld.java
@@ -3,3 +3,10 @@ public class Helloworld{
 		System.out.println("Helloworld");
 	}
 }				
+class Test{
+	public static void main(String[] args) {
+		System.out.println("2018*8269 is ");
+		System.out.println(2018*8269);
+	}
+}
+// 注释是不被编译的
